e34817898668266d12580056ce45f86e6fd2e05b
caporatang <caporatang@naver.com>
2023-02-23 11:37:34 +0900

    HTTP protocol

---
+++ src/main/java/com/multi/pratice/multipratice/springbasic/web/HttpProtocol.java
@@ -0,0 +1,13 @@
+package com.multi.pratice.multipratice.springbasic.web;/**
+* packageName : com.multi.pratice.multipratice.springbasic.web
+* fileName : HttpProtocol
+* author : taeil
+* date : 2023/02/23
+* description : 
+* =======================================================
+* DATE          AUTHOR                      NOTE
+* -------------------------------------------------------
+* 2023/02/23        taeil                   최초생성
+*/
+public class HttpProtocol {
+}
