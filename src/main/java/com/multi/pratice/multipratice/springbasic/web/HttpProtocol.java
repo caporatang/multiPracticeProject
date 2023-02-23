569cfeacdee9e7d177b3d0bdc60ed6beefd581c7
caporatang <caporatang@naver.com>
2023-02-23 11:47:27 +0900

    HTTP protocol

--- src/main/java/com/multi/pratice/multipratice/springbasic/web/HttpProtocol.java
+++ src/main/java/com/multi/pratice/multipratice/springbasic/web/HttpProtocol.java
@@ -10,4 +10,15 @@ package com.multi.pratice.multipratice.springbasic.web;/**
 * 2023/02/23        taeil                   최초생성
 */
 public class HttpProtocol {
+    // HTTP (Hyper Text Transfer Protocol)
+    // RFC 2616에서 규정된 Web에서 데이터를 주고 받는 프로토콜
+    // HTTP는 TCP를 기반으로 한 REST의 특징을 모두 구현하고 있는 Web기반의 프로토콜
+
+    // HTTP는 요청과 응답이 반드시 지정되어있음
+    // 클라이언트가 서버에 요청을 하는 경우 반드시 응답을 대기함
+    // 응답 시간이 길어지거나, 응답이 없는경우 클라이언트 측에서 요청을 취소함
+
+
+
+
 }
