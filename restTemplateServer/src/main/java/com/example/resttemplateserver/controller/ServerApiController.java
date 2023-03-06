f2cacbb4aadcfcaf8ab39d70a33faf8e93b26368
caporatang <caporatang@naver.com>
2023-03-06 15:35:40 +0900

    RestTemplate 네이버 open api

--- restTemplateServer/src/main/java/com/example/resttemplateserver/controller/ServerApiController.java
+++ restTemplateServer/src/main/java/com/example/resttemplateserver/controller/ServerApiController.java
@@ -3,8 +3,18 @@ package com.example.resttemplateserver.controller;
 import com.example.resttemplateserver.dto.Req;
 import com.example.resttemplateserver.dto.User;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.buf.Utf8Encoder;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 /**
  * packageName : com.example.resttemplateserver.controller
@@ -71,4 +81,42 @@ public class ServerApiController {
         return response;
     }
 
+
+    // https://openapi.naver.com/v1/search/local.xml
+    // ?query=\xea\xb0\x88\xeb\xb9\x84\xec\xa7\x91
+    // &display=10
+    // &start=1
+    // &sort=random
+    @GetMapping("/naver")
+    public String naver() {
+
+        String query = "갈비집";
+        //String encode = Base64.getEncoder().encodeToString(query.getBytes(StandardCharsets.UTF_8));
+
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query", "중국집")
+                .queryParam("display", 10)
+                .queryParam("start", 1)
+                .queryParam("sort", "random")
+                .encode(Charset.forName("UTF-8"))
+                .build()
+                .toUri();
+        log.info("uri : {}", uri);
+
+
+        RestTemplate restTemplate = new RestTemplate();
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id", "GlEspjU0WCxbhCo0LX7T")
+                .header("X-Naver-Client-Secret", "GwwMO1cTr2")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+        return result.getBody();
+
+
+    }
 }
