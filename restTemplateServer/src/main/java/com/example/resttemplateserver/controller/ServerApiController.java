84da1eef1081ac1918fc4d3fc5a556fff114c002
caporatang <caporatang@naver.com>
2023-03-15 23:46:14 +0900

    HeapSort

--- restTemplateServer/src/main/java/com/example/resttemplateserver/controller/ServerApiController.java
+++ restTemplateServer/src/main/java/com/example/resttemplateserver/controller/ServerApiController.java
@@ -7,6 +7,7 @@ import org.apache.tomcat.util.buf.Utf8Encoder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -31,6 +32,15 @@ import java.util.Base64;
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+
+    @GetMapping("/index")
+    public String indexTest(Model model) {
+        System.out.println("jsp index 잘 찾아가나요");
+        model.addAttribute("name", "김태일");
+        return "main";
+    }
+
+
 
     // get
     @GetMapping("/hello")
