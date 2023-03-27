5cfcd41b8e25d701c046bc45053f8f3deedf650e
caporatang <caporatang@naver.com>
2023-03-27 19:35:44 +0900

    what is IoStream

---
+++ src/main/java/com/multi/pratice/multipratice/java/basic/oop/iostream/IoStream.java
@@ -0,0 +1,13 @@
+package com.multi.pratice.multipratice.java.basic.oop.iostream;/**
+* packageName : com.multi.pratice.multipratice.java.basic.oop.iostream
+* fileName : IoStream
+* author : taeil
+* date : 2023/03/27
+* description : 
+* =======================================================
+* DATE          AUTHOR                      NOTE
+* -------------------------------------------------------
+* 2023/03/27        taeil                   최초생성
+*/
+public class IoStream {
+}
