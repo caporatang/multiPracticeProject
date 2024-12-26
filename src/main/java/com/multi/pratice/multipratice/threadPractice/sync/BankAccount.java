30dc3812db06863c4170e9bda046cf7f97f8691a
caporatang <caporatang@naver.com>
2024-12-26 19:59:22 +0900

    Chore : RedisSecurityJWT, restTemplateServer gradle 버전 업그레이드

--- src/main/java/com/multi/pratice/multipratice/threadPractice/sync/BankAccount.java
+++ src/main/java/com/multi/pratice/multipratice/threadPractice/sync/BankAccount.java
@@ -2,7 +2,7 @@ package com.multi.pratice.multipratice.threadPractice.sync;
 
 public interface BankAccount {
 
-    boolean withdraw(int amount); // 출금
+    boolean withdraw(int amount) throws InterruptedException; // 출금
 
     int getBalance(); // 잔고 확인
 }
